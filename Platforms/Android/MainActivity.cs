57bd138ff6c3c0039f4242edb9235bc31205928c
Fabian Pavel VELICEA <fabian.velicea@student.unitbv.ro>
2024-11-17 20:44:15 +0200

    Added New Functionalities. Release 1.0.0

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,15 @@
 ﻿using Android.App;
+using Android.Content; // Import this for Intent
 using Android.Content.PM;
 using Android.OS;
+using System.IO; // Import this for StreamWriter
+using System;
 
 namespace TaskMaster
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+       
     }
 }
